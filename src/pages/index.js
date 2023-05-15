0765d8c8342f216b796f5cea65999f97ae8cf224
Varun Seth <8343593+varun-seth@users.noreply.github.com>
2023-05-15 20:28:40 +0530

    💄  layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,25 +1,42 @@
-import React from "react"
-import { Link, graphql } from "gatsby"
+import * as React from "react"
+import { graphql, Link } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+import * as styles from "../components/index.module.css"
 
 const IndexPage = ({ data }) => {
   const puzzles = data.allPuzzlesJson.nodes
 
   return (
-    <main>
-      <h1>Welcome to Puzzles Website</h1>
-      <ul>
-        {puzzles.map(puzzle => (
-          <li key={puzzle.puzzleId}>
-            <h2>
-            <Link to={`/puzzles/${puzzle.puzzleId}`}>{`Puzzle ${puzzle.puzzleId}`}</Link>
-            </h2>
-            <p>{`Difficulty: ${puzzle.difficulty}`}</p>
-            <p>{`Category: ${puzzle.category}`}</p>
-            <p>{`Question: ${puzzle.question}`}</p>
-          </li>
-        ))}
-      </ul>
-    </main>
+    <Layout>
+      <Seo title="Home" />
+      <div className={styles.textCenter}>
+        <StaticImage
+          src="../images/example.png"
+          loading="eager"
+          width={64}
+          quality={95}
+          formats={["auto", "webp", "avif"]}
+          alt=""
+          style={{ marginBottom: `var(--space-3)` }}
+        />
+        <h1>Welcome to Puzzles Website</h1>
+        <ul className={styles.list}>
+          {puzzles.map(puzzle => (
+            <li key={puzzle.puzzleId} className={styles.listItem}>
+              <h2>
+                <Link to={`/puzzles/${puzzle.puzzleId}`} className={styles.listItemLink}>{`Puzzle ${puzzle.puzzleId}`}</Link>
+              </h2>
+              <p className={styles.listItemDescription}>{`Difficulty: ${puzzle.difficulty}`}</p>
+              <p className={styles.listItemDescription}>{`Category: ${puzzle.category}`}</p>
+              <p className={styles.listItemDescription}>{`Question: ${puzzle.question}`}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </Layout>
   )
 }
 
--- src/templates/puzzle.js
+++ src/templates/puzzle.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import { graphql } from 'gatsby'
 
+// Import the layout component
+import Layout from '../components/layout'
+
 export const query = graphql`
   query($puzzleId: String!) {
     puzzlesJson(puzzleId: {eq: $puzzleId}) {
@@ -17,18 +20,21 @@ export const query = graphql`
 `
 
 export default function Puzzle({ data }) {
-const puzzle = data.puzzlesJson
+  const puzzle = data.puzzlesJson
 
-return (
-  <div>
-    <h1>{`Puzzle ${puzzle.puzzleId}`}</h1>
-    <p>{`Difficulty: ${puzzle.difficulty}`}</p>
-    <p>{`Category: ${puzzle.category}`}</p>
-    <p>{`Question: ${puzzle.question}`}</p>
-    <p>{`Hint: ${puzzle.hint}`}</p>
-    <p>{`Answer: ${puzzle.answer}`}</p>
-    <p>{`Solution: ${puzzle.solution}`}</p>
-    {puzzle.image && <img src={puzzle.image} alt={`Puzzle ${puzzle.puzzleId}`}/>}
-  </div>
-)
+  return (
+    // Wrap the content with the layout component
+    <Layout>
+      <div>
+        <h1>{`Puzzle ${puzzle.puzzleId}`}</h1>
+        <p>{`Difficulty: ${puzzle.difficulty}`}</p>
+        <p>{`Category: ${puzzle.category}`}</p>
+        <p>{`Question: ${puzzle.question}`}</p>
+        <p>{`Hint: ${puzzle.hint}`}</p>
+        <p>{`Answer: ${puzzle.answer}`}</p>
+        <p>{`Solution: ${puzzle.solution}`}</p>
+        {puzzle.image && <img src={puzzle.image} alt={`Puzzle ${puzzle.puzzleId}`} />}
+      </div>
+    </Layout>
+  )
 }
