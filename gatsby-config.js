8058a50bef7da24ddbc0c4cfb0f795b3f8cdf5cd
Varun Seth <8343593+varun-seth@users.noreply.github.com>
2023-05-15 20:56:01 +0530

    💄 remove trailing slashes in URL

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,7 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
+  trailingSlash: "never",
   siteMetadata: {
     title: `Gatsby Default Starter`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
