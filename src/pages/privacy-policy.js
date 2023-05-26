daf8e0b47d65e6557fe527bf0d996b3cd7df11aa
Varun Seth <8343593+varun-seth@users.noreply.github.com>
2023-05-27 01:13:04 +0530

    ✨ privacy policy

---
+++ src/pages/privacy-policy.js
@@ -0,0 +1,33 @@
+import React from "react"
+
+const PrivacyPolicy = () => (
+  <div>
+    <h1>Privacy Policy</h1>
+
+    <p>Last updated: 2023-05-27</p>
+
+    <p>We respect and protect the privacy of our users. This Privacy Policy explains that we DO NOT Collect OR Use any user information.</p>
+
+    <h2>Information We Collect: None</h2>
+
+    <p>We does not collect any personal data. We do not collect any information through cookies or other tracking technologies.</p>
+
+    <h2>How We Use Your Information</h2>
+
+    <p>As we do not collect any personal data, we do not use any such information.</p>
+
+    <h2>Data Deletion</h2>
+
+    <p>Since we do not collect or store personal data, there is no personal data to delete.</p>
+
+    <h2>Changes to This Privacy Policy</h2>
+
+    <p>This policy will never change as long as the author (Varun Seth) is alive. Otherwise, any changes will appear on this page.</p>
+
+    <h2>Contact Us</h2>
+
+    <p>If you have any questions about this Privacy Policy, please contact us at: BrainStellarPuzzles@facebook.com</p>
+  </div>
+)
+
+export default PrivacyPolicy
